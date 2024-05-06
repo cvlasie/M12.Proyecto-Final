d10785105bd9ba7b36a0d6d8fbed29d015028c16
cvlasie <clvl@fp.insjoaquimmir.cat>
2024-05-06 17:55:15 +0200

    Hacer Que Funcione El Save Post

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -31,18 +31,43 @@ export const getPost = async (req, res) => {
     try {
         const post = await prisma.post.findUnique({
             where: { id },
-            include:{
+            include: {
                 postDetail: true,
                 user: {
-                    select:{
-                        username:true,
-                        avatar:true,
+                    select: {
+                        username: true,
+                        avatar: true,
                     }
                 }
             }
         });
 
-        res.status(200).json(post);
+        let userId;
+
+        const token = req.cookies?.token;
+
+        if (!token) {
+            userId = null;
+        } else {
+            jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+                if (err) {
+                    userId = null;
+                } else {
+                    userId = payload.id;
+                }
+            });
+        }
+
+        const saved = await prisma.savedPost.findUnique({
+            where: {
+                userId_postId: {
+                    postId: id,
+                    userId,
+                },
+            },
+        });
+
+        res.status(200).json({ ...post, isSaved: saved ? true : false });
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Failed to get post" });
